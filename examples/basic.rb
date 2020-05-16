62120addae09c9d5346b7d338bb2bbd3cab74c71
BuildTools <unconfigured@null.spigotmc.org>
2020-05-16 01:51:03 -0300

    Added begin and rescue to basic.rb Changed inside method name to contained_by? Added containing method

--- examples/basic.rb
+++ examples/basic.rb
@@ -1,5 +1,12 @@
-rect_a = MRGSS::Rectangle.new(0, 0, 10, 10)
-rect_b = MRGSS::Rectangle.new(1, 1,  8,  8)
-print rect_b.inside(rect_a)
+begin
+    rect_a = MRGSS::Rectangle.new(0, 0, 10, 10)
+    rect_b = MRGSS::Rectangle.new(1, 1,  8,  8)
+    
+    print rect_b.contained_by?(rect_a)
+    puts "\n"
+    print rect_a.containing(rect_b)
+rescue
+    print "falló"
+end
 
 
--- mrblib/mrgss_rect.rb
+++ mrblib/mrgss_rect.rb
@@ -1,9 +1,12 @@
 module MRGSS
     class Rectangle
-        def inside(other)
+        def contained_by?(other)
             starts_inside = self.x > other.x && self.y > other.y
             ends_inside = (self.width + self.x < other.width + other.x) && (self.height + self.y < other.height + other.y)
             return  starts_inside && ends_inside
+        end
+        def containing(other)
+            other.contained_by?(self)
         end
     end
 end
